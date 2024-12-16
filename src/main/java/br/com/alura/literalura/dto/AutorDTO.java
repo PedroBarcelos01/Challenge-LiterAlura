2bbddb6c77823c2bab1879807bd04cfc66a1b877
Pedro Barcelos <pedromiguel1203@hotmail.com>
2024-12-16 16:17:34 -0300

    Delete src directory

--- src/main/java/br/com/alura/literalura/dto/AutorDTO.java
+++
@@ -1,8 +0,0 @@
-package br.com.alura.literalura.dto;
-
-import com.fasterxml.jackson.annotation.JsonAlias;
-
-public record AutorDTO(@JsonAlias("name") String autor,
-                       @JsonAlias("birth_year") Integer dataNascimento,
-                       @JsonAlias("death_year") Integer dataMorte) {
-}
